543fc8e63d082c5fe227b87d6e228437f828fe2b
lcaron4910 <49002076+lcaron4910@users.noreply.github.com>
2019-03-30 12:13:35 +0100

    Projet

---
+++ personnage.cs
@@ -0,0 +1,14 @@
+class Personnage{
+	public void Frapper(Personnage personnage, int force){
+		
+	}
+
+	public void deplacer(Direction direction){
+
+
+	public lieu regarder(Direction direction,int distance){
+		
+
+	}
+
+}
